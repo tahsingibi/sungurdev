93fe96455de384dab4c2fcdaf5dd7dbbe5bddb9f
Tahsin Sungur <35434725+tahsingibi@users.noreply.github.com>
2024-08-25 18:59:35 +0300

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
       <body>
         <MainLayout>
           {children}
