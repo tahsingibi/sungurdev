1c8bf17fc5e8ad6fc1096628577eed05deaf1ff5
Tahsin Sungur <35434725+tahsingibi@users.noreply.github.com>
2024-10-31 15:27:38 +0300

    inline code font-weight edit

--- src/mdx-components/inline-code/index.js
+++ src/mdx-components/inline-code/index.js
@@ -1,3 +1,3 @@
 export default function InlineCode({ children }) {
-  return <code className="font-mono text-zinc-400">{children}</code>;
+  return <code className="font-mono font-light text-zinc-400">{children}</code>;
 }
