f036bd8f46e1cf9ec3182969954c7330c1bc93cd
Tahsin Sungur <sungur@sungurdev.local>
2025-11-26 19:23:44 +0300

    fix: yt-dlp-downloader.mdx content edit

--- src/components/active-link/index.js
+++ src/components/active-link/index.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { usePathname } from 'next/navigation';
 import * as NextLink from 'next/link';
 
-export default function Link({ children, ...props }) {
+export default function Link({ children, prefetch = false, ...props }) {
   const path = usePathname();
   const { href = '' } = props;
   const isActive = path === href;
   return (
-    <NextLink {...props} data-active={isActive}>
+    <NextLink prefetch={prefetch} {...props} data-active={isActive}>
       {children}
     </NextLink>
   );
